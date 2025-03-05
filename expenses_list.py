76fb118bae9f7d2a689fce020223505315c62bd2
aleksigl <112393919+aleksigl@users.noreply.github.com>
2025-03-05 18:10:43 +0100

    Delete expenses_list.py

--- expenses_list.py
+++
@@ -1,8 +0,0 @@
-expenses = {
-    'expense_name' : 'Medicine',
-    'category' : 'Doggo',
-    'price' : '450',
-    'account' : 'Common',
-    'date' : 2025-02-25,
-    'comment' : 'Monthly standards...'
-}
