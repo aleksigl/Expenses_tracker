d6a94d31dff859f7b47a7b19f16078a3b582f58a
aleksigl <112393919+aleksigl@users.noreply.github.com>
2025-03-05 20:28:18 +0100

    Adjust code to PEP8

--- forms.py
+++ forms.py
@@ -10,8 +10,21 @@ def integer_only(form, field):
 
 class ExpensesForm(FlaskForm):
     expense_name = StringField('Expense Name', validators=[DataRequired()])
-    category = SelectField('Category', choices=[('mortgage', 'Mortgage'), ('bills', 'Bills'), ('food', 'Food'), ('doggo', 'Doggo'), ('car', 'Car'), ('hobbies', 'Hobbies'), ('travel', 'Travel'), ('others', 'Others')], validators=[DataRequired()])
+    category = SelectField('Category', choices=[
+        ('mortgage', 'Mortgage'),
+        ('bills', 'Bills'),
+        ('food', 'Food'),
+        ('doggo', 'Doggo'),
+        ('car', 'Car'),
+        ('hobbies', 'Hobbies'),
+        ('travel', 'Travel'),
+        ('others', 'Others')
+    ], validators=[DataRequired()])
     price = IntegerField('Price (PLN)',  validators=[NumberRange(min=0, max=10000), DataRequired(), integer_only])
-    account = SelectField('Account', choices=[('wifeys', "Wifey's"), ('husbands', "Husband's"), ('common', 'Common')], validators=[DataRequired()])
+    account = SelectField('Account', choices=[
+        ('wifeys', "Wifey's"),
+        ('husbands', "Husband's"),
+        ('common', 'Common')
+    ], validators=[DataRequired()])
     date = DateField('Date (YYYY-MM-DD)', format='%Y-%m-%d', validators=[DataRequired()])
     comment = TextAreaField('Comment', validators=[Optional(), Length(max=500)])
